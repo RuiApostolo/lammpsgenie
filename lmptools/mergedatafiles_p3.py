ae34e7d0c53c1e927661304d1a14cb00f3403412
Rui Apóstolo <ruiapostolo@gmail.com>
2022-04-29 16:53:13 +0100

    Some progress on mergedatafiles_p3.py

--- lmptools/mergedatafiles_p3.py
+++ lmptools/mergedatafiles_p3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #  import lmptools.readfiles as rdfl
-#  import lmptools.atoms as atoms
+import lmptools.atoms as atoms
 #  import lmptools.commondata_p3 as cdp3
 from yaml import full_load
 from sys import argv
@@ -22,8 +22,8 @@ def readInputFile(args):
         if len(args) < 2:
             for filename in ['merge.yaml', 'merge.yml']:
                 try:
-                    settings = _openYaml(filename)
-                    break
+                    return _openYaml(filename)
+
                 except IOError:
                     continue
             else:
@@ -32,16 +32,24 @@ def readInputFile(args):
             raise TooManyArguments
         else:
             filename = args[1]
-            settings = _openYaml(filename)
+            return _openYaml(filename)
 
     except MissingSettingsFile:
         # TODO expand instructions
         _myExit("Missing settings file: merge.yaml or merge.yml", 3)
 
     except TooManyArguments:
+        # TODO expand instructions
         _myExit("This script takes only one argument, the settings file.", 4)
 
-    return settings
+
+def readTopologies(files):
+    topologies = {}
+    for file in files:
+        topologies[file] = atoms.getAllAtomData(file['filename'])
+        # check max Z
+        #  a[max(a, key=lambda v: a[v]['z'])]['z']
+    return topologies
 
 
 def _myExit(message, code):
@@ -50,13 +58,21 @@ def _myExit(message, code):
 
 
 def _openYaml(ifile):
-    print(ifile)
     with open(ifile, 'r') as f:
         settings = full_load(f)
-    print(settings)
     return settings
 
 
+def _limit(function, coordinate, topology):
+    return topology['atomdata'][function(
+        topology, key=lambda atom:
+            topology['atomdata'][atom][coordinate])][coordinate]
+
+
 if __name__ == '__main__':
-    readInputFile()  # pragma: no cover
+    files = readInputFile()  # pragma: no cover
+    # read files
+    topologies = readTopologies(files)
+    # write new datafile
+
     # TODO: consider pytest-console-scripts tests
