20bdb3cac6525e004e25ff3bab979e25a71d3a47
Rui Apóstolo <ruiapostolo@gmail.com>
2022-05-12 17:20:48 +0100

    Cleanup of __init__.py

--- lmptools/__init__.py
+++ lmptools/__init__.py
@@ -1,7 +1,1 @@
 # __init__.py
-from lmptools.commondata import *
-from lmptools.atoms import *
-from lmptools.readfiles import *
-from lmptools.mergedatafiles import *
-from lmptools._version import __version__
-from lmptools._name import _name
