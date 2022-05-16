8d7d107278205fa22757962169955fd3dee49141
Rui Apóstolo <ruiapostolo@gmail.com>
2022-05-16 15:13:21 +0100

    Added package __doc__

--- lammpsgenie/__init__.py
+++ lammpsgenie/__init__.py
@@ -1,4 +1,11 @@
 # __init__.py
+"""
+This package is a collection of tools for handling LAMMPS data and dump files.
+
+Please refer to more information in the README.md provided at:
+github.com/RuiApostolo/lmpdtmrg
+"""
+
 from lammpsgenie.commondata import *
 from lammpsgenie.atoms import *
 from lammpsgenie.readfiles import *
